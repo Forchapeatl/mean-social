53c86e0bdd76fdc98e6a7b0abc59b0a8bba48a19
FORCHA <forchapearl1@gmail.com>
2021-09-23 20:07:25 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-	database: 'mongodb://user123:12345678@//merncrudapp1.netlify.app:27017/meanauth',
+	database: 'mongodb://user123:12345678@merncrudapp1.netlify.app:3501/meanauth',
 	secret: 'seeks'
 }
